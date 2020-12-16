dcd87e1339c87ca5defa2221a8391d3c9b2a1d42
Vinay Rao <vinayrao7039@gmail.com>
2020-12-16 14:57:25 +0530

    Drawer Navigation Commit

--- app/src/main/java/com/example/timetablegenerator/MainActivity.java
+++ app/src/main/java/com/example/timetablegenerator/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home,R.id.nav_c_r_contact, R.id.nav_about_us, R.id.nav_c_r_contact, R.id.nav_faculty_contact,R.id.nav_help)
+                R.id.nav_home,R.id.nav_tt, R.id.nav_faculty_contact, R.id.nav_c_r_contact, R.id.nav_help,R.id.nav_about_us)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
